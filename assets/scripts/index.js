07539510c649eb54d461b8fbf20e12e32d6fe14f
Yemata Ferry <yemataferry@gmail.com>
2024-01-06 07:42:30 +0100

    script functionalities added

---
+++ assets/scripts/index.js
@@ -0,0 +1,24 @@
+const sounds = ['applause', 'siren', 'gasp', 'tada', 'victory', 'wrong', 'boo', 'gunshot']
+
+sounds.forEach(sound => {
+  const btn = document.createElement('button')
+  btn.classList.add('btn')
+
+  btn.innerText = sound
+
+  btn.addEventListener('click', () => {
+    stopSongs()
+
+    document.getElementById(sound).play()
+  })
+
+  document.getElementById('buttons').appendChild(btn)
+})
+
+function stopSongs () {
+  sounds.forEach (sound => {
+    const song = document.getElementById(sound)
+    song.pause()
+    song.currentTime = 0
+  })
+}
